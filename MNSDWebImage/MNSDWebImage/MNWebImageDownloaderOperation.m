49a98cd14415d6118fcf40eb2b6b4b8d03675e9d
蔡钰 <1626179102@qq.com>
2017-05-15 17:58:31 +0800

    图片下载的实现,并建立操作缓存池解决重复添加操作的问题

---
+++ MNSDWebImage/MNSDWebImage/MNWebImageDownloaderOperation.m
@@ -0,0 +1,41 @@
+//
+//  MNWebImageDownloaderOperation.m
+//  MNSDWebImage
+//
+//  Created by 蔡钰 on 2017/5/15.
+//  Copyright © 2017年 蔡钰. All rights reserved.
+//
+
+#import "MNWebImageDownloaderOperation.h"
+
+@implementation MNWebImageDownloaderOperation
+
+- (void)main {
+    //线程睡眠,模拟网络延时
+    [NSThread sleepForTimeInterval:5];
+    
+    NSLog(@"%@",[NSThread currentThread]);
+    
+    NSURL *url = [NSURL URLWithString:self.urlStr];
+    NSData *data = [NSData dataWithContentsOfURL:url];
+    UIImage *image = [UIImage imageWithData:data];
+    
+    if (self.successBlock) {
+        //切换主线程
+        [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+            self.successBlock(image);
+        }];
+    }
+
+}
+
++(instancetype)webImageDownloaderOperationWithURLStr:(NSString *)urlStr andSuccessBlock:(void(^)(UIImage *image)) successBlock {
+    
+    MNWebImageDownloaderOperation *op = [[MNWebImageDownloaderOperation alloc] init];
+    op.urlStr = urlStr;
+    op.successBlock = successBlock;
+    
+    return op;
+}
+
+@end
--- MNSDWebImage/MNSDWebImage/ViewController.m
+++ MNSDWebImage/MNSDWebImage/ViewController.m
@@ -7,8 +7,24 @@
 //
 
 #import "ViewController.h"
+#import "MNWebImageDownloaderOperation.h"
 
 @interface ViewController ()
+
+/**
+ 操作缓存池
+ */
+@property (nonatomic,strong) NSMutableDictionary<NSString *, MNWebImageDownloaderOperation *> *opCache;
+
+/**
+ 全局队列
+ */
+@property (nonatomic,strong) NSOperationQueue *queue;
+
+/**
+ 展示图片的imageView
+ */
+@property (nonatomic,weak) UIImageView *imgView;
 
 @end
 
@@ -16,9 +32,63 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(120, 200, 80, 80)];
+    imageView.backgroundColor = [UIColor darkGrayColor];
+    [self.view addSubview:imageView];
+    
+    self.imgView = imageView;
+    
+    
 }
 
+-(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    
+    NSString *urlStr = @"http://p16.qhimg.com/dr/48_48_/t0125e8d438ae9d2fbb.png";
+    
+    if ([_opCache valueForKey:urlStr]) {
+        
+        NSLog(@"图片下载中");
+    
+        return;
+    }
+    
+    MNWebImageDownloaderOperation *op = [MNWebImageDownloaderOperation webImageDownloaderOperationWithURLStr:@"http://p16.qhimg.com/dr/48_48_/t0125e8d438ae9d2fbb.png" andSuccessBlock:^(UIImage *image) {
+        
+        self.imgView.image = image;
+        
+        NSLog(@"图片下载成功");
+        
+        [self.opCache removeObjectForKey:urlStr];
+        
+    }];
+    
+    //将操作添加到缓存池中
+    [self.opCache setValue:op forKey:urlStr];
+    
+    [self.queue addOperation:op];
+    
+}
+
+//懒加载缓存池
+-(NSMutableDictionary<NSString *,MNWebImageDownloaderOperation *> *)opCache {
+    
+    if (!_opCache) {
+        _opCache = [[NSMutableDictionary alloc] init];
+    }
+
+    return _opCache;
+}
+
+//懒加载操作队列
+-(NSOperationQueue *)queue {
+    
+    if (!_queue) {
+        _queue = [[NSOperationQueue alloc] init];
+    }
+
+    return _queue;
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
